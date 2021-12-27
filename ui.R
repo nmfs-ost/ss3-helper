a7847f14b995bee1766c37ca271ff0edc581ae3a
Kathryn Doering <kathryn.doering@noaa.gov>
2021-12-27 11:51:29 -0500

    style: run styler::style_active_file() on 2 files
    To make the source code formatting standardized

--- ui.R
+++ ui.R
@@ -7,42 +7,47 @@ ui <- pageWithSidebar(
   # Application title
   headerPanel("Stock Synthesis Selectivity"),
 
-  #Sidebar controls to select selectivity type and parameters
+  # Sidebar controls to select selectivity type and parameters
   sidebarPanel(
     selectInput("type", "Type:",
-                choices = c("Logistic (1)",
-                            "Double Normal (24)")),
-
+      choices = c(
+        "Logistic (1)",
+        "Double Normal (24)"
+      )
+    ),
     sliderInput("range", "Lengths:",
-                min = 0, max = 100, value = c(0,50)),
-
+      min = 0, max = 100, value = c(0, 50)
+    ),
     h4("Enter parameters below (slider or box)"),
     conditionalPanel(
       condition = "input.type == 'Logistic (1)'",
       fluidRow(
-        column(8,
-          sliderInput("par1", "Parameter 1:", 0,100,10,0.1),
-          sliderInput("par2", "Parameter 2:", 0,100,1,0.1)
+        column(
+          8,
+          sliderInput("par1", "Parameter 1:", 0, 100, 10, 0.1),
+          sliderInput("par2", "Parameter 2:", 0, 100, 1, 0.1)
         ),
-        column(4,
-          numericInput("par1N","Parameter 1:",10),
-          numericInput("par2N","Parameter 2:",1)
+        column(
+          4,
+          numericInput("par1N", "Parameter 1:", 10),
+          numericInput("par2N", "Parameter 2:", 1)
         )
       )
     ),
-
     conditionalPanel(
       condition = "input.type == 'Double Normal (24)'",
       fluidRow(
-        column(8,
-          sliderInput("par.a", "PEAK:",0, 100, 25, 0.1),
+        column(
+          8,
+          sliderInput("par.a", "PEAK:", 0, 100, 25, 0.1),
           sliderInput("par.b", "TOP:", -5, 5, 0, 0.1),
           sliderInput("par.c", "ASC-WIDTH:", -5, 10, 3, 0.1),
           sliderInput("par.d", "DESC-WIDTH:", -5, 10, 3, 0.1),
           sliderInput("par.e", "INIT:", 0, 1, 0.1, 0.05),
           sliderInput("par.f", "FINAL:", 0, 1, 0.9, 0.05)
         ),
-        column(4,
+        column(
+          4,
           numericInput("par.aN", "PEAK", 25),
           numericInput("par.bN", "TOP", 0),
           numericInput("par.cN", "ASC-WIDTH", 3),
@@ -53,11 +58,8 @@ ui <- pageWithSidebar(
       )
     )
   ),
-
   mainPanel(
     h3(textOutput("caption")),
-
     plotOutput("selPlot")
   )
-
 )
