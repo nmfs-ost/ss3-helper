6acc8fabf3ec0c63bdbd8f6b1496bb0616db7a35
Kathryn Doering <kathryn.doering@noaa.gov>
2021-12-30 11:29:35 -0500

    refactor: move code into more functions/components
    This should help keep the code more organized as the codebase grows. Note that modules could also be used to organize the app if need be.

---
+++ selex_fxns.R
@@ -0,0 +1,114 @@
+#selectivity functions
+
+#' Calculate values for length logistic selectivity
+#'
+#' @param len A vector of lengths or ages
+#' @param a The inflection point
+#' @param b The 95% width
+#' @return The selectivity curve as a vector
+logistic1.fn <- function(len, a, b) {
+  neglog19 <- -1 * log(19)
+  denom <- 1. + exp(neglog19 * (len - a) / b)
+  sel <- 1 / denom
+  return(sel)
+}
+
+#' Calculate values for double normal selectivity
+#'
+#' @param x A vector of lengths or ages
+#' @param a The peak
+#' @param b The top
+#' @param c The ascending width
+#' @param d The Descending width
+#' @param e The initial value
+#' @param f The final value
+#' @param use_e_999 Is -999 used for the initial value?
+#' @param use_f_999 Is -999 used for the final value?
+#' @return The double normal selectivity curve given the parameters as a vector
+doubleNorm24.fn <- function(x, a, b, c, d, e, f, use_e_999, use_f_999) {
+  # TODO: check if function not handling f < -1000 correctly (and -999 vals)
+  if(use_e_999) {
+    e <- -999
+  }
+  if(use_f_999) {
+    f <- -999
+  }
+  if (e == 0) { # Avoid errors on the bounds
+    e <- 1 - 0.999955 # an input that results in approx -10
+  }
+  if (e == 1) {
+    e <- 0.999955 # an input that results in approx 10
+  }
+  if (e > 0) {
+    e <- log(e / (1 - e)) # transform input to logit
+  }
+
+  if (f == 0) { # Avoid errors on the bounds
+    f <- 1 - 0.999955 # an input that results in approx -10
+  }
+  if (f == 1) {
+    f <- 0.999955 # an input that results in approx 10
+  }
+  if(f > 0) {
+    f <- log(f / (1 - f)) # transform input to logit
+  }
+  sel <- rep(NA, length(x))
+  startbin <- 1
+  peak <- a
+  upselex <- exp(c)
+  downselex <- exp(d)
+  final <- f
+  if (e < -1000) {
+    j1 <- -1001 - round(e)
+    sel[1:j1] <- 1e-06
+  }
+  if (e >= -1000) {
+    j1 <- startbin - 1
+    if (e > -999) {
+      point1 <- 1 / (1 + exp(-e))
+      t1min <- exp(-(x[startbin] - peak)^2 / upselex)
+    }
+  }
+  if (f < -1000) {
+    j2 <- -1000 - round(f)
+  }
+  if (f >= -1000) {
+    j2 <- length(x)
+  }
+  bin_width <- x[2] - x[1]
+  peak2 <- peak + bin_width + (0.99 * x[j2] - peak - bin_width) / (1 +
+                                                                     exp(-b))
+  if (f > -999) {
+    point2 <- 1 / (1 + exp(-final))
+    t2min <- exp(-(x[j2] - peak2)^2 / downselex)
+  }
+  t1 <- x - peak
+  t2 <- x - peak2
+  join1 <- 1 / (1 + exp(-(20 / (1 + abs(t1))) * t1))
+  join2 <- 1 / (1 + exp(-(20 / (1 + abs(t2))) * t2))
+  if (e > -999) {
+    asc <- point1 + (1 - point1) * (exp(-t1^2 / upselex) -
+                                      t1min) / (1 - t1min)
+  }
+  if (e <= -999) {
+    asc <- exp(-t1^2 / upselex)
+  }
+  if (f > -999) {
+    dsc <- 1 + (point2 - 1) * (exp(-t2^2 / downselex) -
+                                 1) / (t2min - 1)
+  }
+  if (f <= -999) {
+    dsc <- exp(-(t2)^2 / downselex)
+  }
+  idx.seq <- (j1 + 1):j2
+  sel[idx.seq] <- asc[idx.seq] * (1 - join1[idx.seq]) + join1[idx.seq] * (1 -
+                                                                            join2[idx.seq] + dsc[idx.seq] * join2[idx.seq])
+  if (startbin > 1 && e >= -1000) {
+    sel[1:startbin] <- (x[1:startbin] / x[startbin])^2 *
+      sel[startbin]
+  }
+  if (j2 < length(x)) {
+    sel[(j2 + 1):length(x)] <- sel[j2]
+  }
+  return(sel)
+}
--- ui.R
+++ ui.R
@@ -1,160 +1,169 @@
+# dependencies ----
 library(shiny)
 library(bslib)
 library(shinydashboard)
 library(shinyjs)
 
-ui <- dashboardPage(
-  #withMathJax(), # math jax doesn't seem to work with shinydashboard?
-  skin = "blue", # the theme
-  # Application title ----
-  dashboardHeader(title = "SS3 Helper",
-    # add help link
-    dropdownMenu(
-      type = "notifications",
-      headerText = strong("HELP"),
-      icon = icon("question-circle"),
-      badgeStatus = NULL,
-      notificationItem(
-        text = "Ask a question",
-        icon = icon("question"),
-        href = "https://vlab.noaa.gov/web/stock-synthesis/public-forums"
-      ),
-      notificationItem(
-        text = "Report an issue",
-        icon = icon("bug"),
-        href = "https://github.com/r4ss/shiny_selex/issues"
-      )
-    ),
-    # add a link to the source code
-    tags$li(
-      a(
-        tags$img(src="https://raw.githubusercontent.com/nmfs-fish-tools/nmfs-fish-tools.github.io/master/static/icons8-code-fork-24.png", height="30"),
-        href = "https://github.com/r4ss/shiny_selex",
-        title = "See the source code",
-      ),
-      class = "dropdown"
-    ),
-    # add noaa branding
-    tags$li(
-      a(
-        tags$img(src="NOAA-Logo-Without-Ring.png", height="30"),
-        href = "https://www.noaa.gov",
-        title = "Learn more about NOAA",
-      ),
-      class = "dropdown"
-    )
-  ),
-  # sidebar ----
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Selectivity", tabName = "Selectivity", icon = icon("ship")),
-      # menuItem("Time-Varying Parameters", tabName = "tv", icon = icon("clock")),
-      menuItem("Info", tabName = "info", icon = icon("info"))
-    )
-  ),
-  # body ----
-  dashboardBody(
-    useShinyjs(),
-    tabItems(
-      tabItem(tabName = "Selectivity",
-        sidebarLayout(
-        # Sidebar controls to select selectivity type and parameters
-          sidebarPanel(
-            selectInput("type", "Selectivity Pattern:",
-              choices = c(
-                "Logistic (1)",
-                "Double Normal (24 length, 20 age)"
-              )
-            ),
-            sliderInput("range", "Length or Age Range:",
-              min = 0, max = 100, value = c(0, 50)
-            ),
-            h2("Enter parameters below (slider or box)"),
-            conditionalPanel(
-              condition = "input.type == 'Logistic (1)'",
-              fluidRow(
-                column(
-                  8,
-                  sliderInput("par1", "Parameter 1 (p1), Size at inflection:",
-                              0, 100, 10, 0.1),
-                  sliderInput("par2", "Parameter 2 (p2), Width for 95% selection:",
-                              -100, 100, 1, 0.1)
+# header ----
+header <-
+dashboardHeader(title = "SS3 Helper",
+                # add help link
+                dropdownMenu(
+                  type = "notifications",
+                  headerText = strong("HELP"),
+                  icon = icon("question-circle"),
+                  badgeStatus = NULL,
+                  notificationItem(
+                    text = "Ask a question",
+                    icon = icon("question"),
+                    href = "https://vlab.noaa.gov/web/stock-synthesis/public-forums"
+                  ),
+                  notificationItem(
+                    text = "Report an issue",
+                    icon = icon("bug"),
+                    href = "https://github.com/r4ss/shiny_selex/issues"
+                  )
                 ),
-                column(
-                  4,
-                  numericInput("par1N", "Parameter 1:", 10),
-                  numericInput("par2N", "Parameter 2:", 1)
+                # add a link to the source code
+                tags$li(
+                  a(
+                    tags$img(src="https://raw.githubusercontent.com/nmfs-fish-tools/nmfs-fish-tools.github.io/master/static/icons8-code-fork-24.png", height="30"),
+                    href = "https://github.com/r4ss/shiny_selex",
+                    title = "See the source code",
+                  ),
+                  class = "dropdown"
+                ),
+                # add noaa branding
+                tags$li(
+                  a(
+                    tags$img(src="NOAA-Logo-Without-Ring.png", height="30"),
+                    href = "https://www.noaa.gov",
+                    title = "Learn more about NOAA",
+                  ),
+                  class = "dropdown"
                 )
-              )#,
-              # not sure how to get eqns to work, with shiny dashboard, but would
-              # be nice to add
-              # fluidRow(h2("Equation:")),
-              # fluidRow(helpText('$$S = \\frac{1}{1+e^{\\frac{-ln(19)(L - p1)}{p2}}}$$')
-              # )
-            ),
-            conditionalPanel(
-              condition = "input.type == 'Double Normal (24 length, 20 age)'",
-              fluidRow(
-                column(
-                  8,
-                  sliderInput("par.a", "Length or Age for Peak:", 0, 100, 25, 0.1),
-                  sliderInput("par.b", "Width of Top:", -5, 5, 0, 0.1),
-                  sliderInput("par.c", "Ascending width:", -5, 10, 3, 0.1),
-                  sliderInput("par.d", "Descending width:", -5, 10, 3, 0.1),
-                  sliderInput("par.e", "Initial Selectivity:", 0, 1, 0.1, 0.05),
-                  sliderInput("par.f", "Final Selectivity:", 0, 1, 0.9, 0.05)
+)
+
+# sidebar ----
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Selectivity", tabName = "Selectivity", icon = icon("ship")),
+    # menuItem("Time-Varying Parameters", tabName = "tv", icon = icon("clock")),
+    menuItem("Info", tabName = "info", icon = icon("info"))
+  )
+)
+
+# body ----
+body <- dashboardBody(
+  useShinyjs(),
+  tabItems(
+    tabItem(tabName = "Selectivity",
+            sidebarLayout(
+              # Sidebar controls to select selectivity type and parameters
+              sidebarPanel(
+                selectInput("type", "Selectivity Pattern:",
+                            choices = c(
+                              "Logistic (1)",
+                              "Double Normal (24 length, 20 age)"
+                            )
                 ),
-                column(
-                  4,
-                  numericInput("par.aN", "Peak", 25),
-                  numericInput("par.bN", "Top", 0),
-                  numericInput("par.cN", "Ascending width", 3),
-                  numericInput("par.dN", "Descending width", 3),
-                  numericInput("par.eN", "Initial", 0.1),
-                  numericInput("par.fN", "Final", 0.9)
+                sliderInput("range", "Length or Age Range:",
+                            min = 0, max = 100, value = c(0, 50)
+                ),
+                h2("Enter parameters below (slider or box)"),
+                conditionalPanel(
+                  condition = "input.type == 'Logistic (1)'",
+                  fluidRow(
+                    column(
+                      8,
+                      sliderInput("par1", "Parameter 1 (p1), Size at inflection:",
+                                  0, 100, 10, 0.1),
+                      sliderInput("par2", "Parameter 2 (p2), Width for 95% selection:",
+                                  -100, 100, 1, 0.1)
+                    ),
+                    column(
+                      4,
+                      numericInput("par1N", "Parameter 1:", 10),
+                      numericInput("par2N", "Parameter 2:", 1)
+                    )
+                  )#,
+                  # not sure how to get eqns to work, with shiny dashboard, but would
+                  # be nice to add
+                  # fluidRow(h2("Equation:")),
+                  # fluidRow(helpText('$$S = \\frac{1}{1+e^{\\frac{-ln(19)(L - p1)}{p2}}}$$')
+                  # )
+                ),
+                conditionalPanel(
+                  condition = "input.type == 'Double Normal (24 length, 20 age)'",
+                  fluidRow(
+                    column(
+                      8,
+                      sliderInput("par.a", "Length or Age for Peak:", 0, 100, 25, 0.1),
+                      sliderInput("par.b", "Width of Top:", -5, 5, 0, 0.1),
+                      sliderInput("par.c", "Ascending width:", -5, 10, 3, 0.1),
+                      sliderInput("par.d", "Descending width:", -5, 10, 3, 0.1),
+                      sliderInput("par.e", "Initial Selectivity:", 0, 1, 0.1, 0.05),
+                      sliderInput("par.f", "Final Selectivity:", 0, 1, 0.9, 0.05)
+                    ),
+                    column(
+                      4,
+                      numericInput("par.aN", "Peak", 25),
+                      numericInput("par.bN", "Top", 0),
+                      numericInput("par.cN", "Ascending width", 3),
+                      numericInput("par.dN", "Descending width", 3),
+                      numericInput("par.eN", "Initial", 0.1),
+                      numericInput("par.fN", "Final", 0.9)
+                    )
+                  ),
+                  fluidRow(
+                    column(6,
+                           checkboxInput("use_999_init", "Use -999 for Initial")
+                    ),
+                    column(6,
+                           checkboxInput("use_999_fin", "Use -999 for Final")
+                    )
+                  )
                 )
               ),
-              fluidRow(
-                column(6,
-                  checkboxInput("use_999_init", "Use -999 for Initial")
-                ),
-                column(6,
-                   checkboxInput("use_999_fin", "Use -999 for Final")
+              mainPanel(
+                h3(textOutput("caption")),
+                plotOutput("selPlot"),
+                conditionalPanel(
+                  condition = "input.type == 'Double Normal (24 length, 20 age)'",
+                  fluidRow(
+                    h2("Additional options for double normal selectivity"),
+                    tags$ul(
+                      tags$li("For initial, an additional option not illustrated is to use a value <-1000, which ignores the initial selectivity algorithm and sets selectivity equal to 1.0e-06 for size bins 1 through bin = -1001 – value. So a value of –1003 would set selectivity to a nil level for bins 1 through 2 and begin using the modeled selectivity in bin 3."),
+                      tags$li("For final, an additional option not illustrated is to use a value <-1000. This sets selectivity as constant for bins greater than bin number = -1000 – value. ")
+                    )
+                  )
                 )
               )
             )
-          ),
-          mainPanel(
-            h3(textOutput("caption")),
-            plotOutput("selPlot"),
-            conditionalPanel(
-              condition = "input.type == 'Double Normal (24 length, 20 age)'",
-              fluidRow(
-                h2("Additional options for double normal selectivity"),
-                tags$ul(
-                  tags$li("For initial, an additional option not illustrated is to use a value <-1000, which ignores the initial selectivity algorithm and sets selectivity equal to 1.0e-06 for size bins 1 through bin = -1001 – value. So a value of –1003 would set selectivity to a nil level for bins 1 through 2 and begin using the modeled selectivity in bin 3."),
-                  tags$li("For final, an additional option not illustrated is to use a value <-1000. This sets selectivity as constant for bins greater than bin number = -1000 – value. ")
-                )
-              )
-            )
-          )
-        )
-      ),
-      # tabItem(
-      #   tabName = "tv",
-      #   p("To be developed")
-      # ),
-      tabItem(
-        tabName = "info",
-        h2("Authors"),
-        p("Andrea Havron, Allan Hicks, Ian Taylor, and Kathryn Doering"),
-        h2("Links to learn more about NOAA"),
-        tags$ul(
-          tags$li(a(href = "https://www.commerce.gov/","U.S. Department of Commerce")),
-          tags$li(a(href = "https://www.noaa.gov","National Oceanographic and Atmospheric Administration (NOAA)")),
-          tags$li(a(href = "https://www.fisheries.noaa.gov/","NOAA Fisheries"))
-        )
+    ),
+    # tabItem(
+    #   tabName = "tv",
+    #   p("To be developed")
+    # ),
+    tabItem(
+      tabName = "info",
+      h2("Authors"),
+      p("Andrea Havron, Allan Hicks, Ian Taylor, and Kathryn Doering"),
+      h2("Links to learn more about NOAA"),
+      tags$ul(
+        tags$li(a(href = "https://www.commerce.gov/","U.S. Department of Commerce")),
+        tags$li(a(href = "https://www.noaa.gov","National Oceanographic and Atmospheric Administration (NOAA)")),
+        tags$li(a(href = "https://www.fisheries.noaa.gov/","NOAA Fisheries"))
       )
     )
   )
+)
+
+# define ui ----
+ui <- dashboardPage(
+  #withMathJax(), # math jax doesn't seem to work with shinydashboard?
+  skin = "blue", # the theme
+  header,
+  sidebar,
+  body
 )
